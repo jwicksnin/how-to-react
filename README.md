618489ab74f983256cbbbeded3f444eb7a512cfb
jwicksnin <jwicksnin@gmail.com>
2016-03-27 15:45:54 -0700

    Start the project simply, with Webpack.

---
+++ README.md
@@ -0,0 +1,3 @@
+# How-To-React
+
+A fun, interactive, minimally-frustrating way to pick up React.js (and webpack, jsx, and ES2015 - aka ES6).
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// TODO: use export default instead
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
